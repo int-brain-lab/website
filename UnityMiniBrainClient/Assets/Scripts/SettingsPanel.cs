b361def26994f1001f52398c671d2f3d8204a12d
Dan Birman <danbirman@gmail.com>
2022-06-27 09:00:51 -0700

    Adding unity mini brain client
    Initial commit of Unity code with javascript alerts when you click on probe objects

---
+++ UnityMiniBrainClient/Assets/Scripts/SettingsPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SettingsPanel : MonoBehaviour
+{
+    [SerializeField] private BrainCameraController cameraController;
+    [SerializeField] private GameObject settingsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && EventSystem.current.IsPointerOverGameObject())
+            cameraController.SetControlBlock(true);
+
+        if (Input.GetMouseButtonUp(0))
+            cameraController.SetControlBlock(false);
+
+        if (Input.GetKeyDown(KeyCode.S))
+            settingsPanel.SetActive(settingsPanel.activeSelf);
+    }
+}
