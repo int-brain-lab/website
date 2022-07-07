6a52f2ec5125d08880201c87fb2f96dc4495d3a2
Cyrille Rossant <rossant@users.noreply.github.com>
2022-07-07 10:28:38 +0200

    Code styling

--- plots/static_plots.py
+++ plots/static_plots.py
@@ -1,3 +1,7 @@
+# -------------------------------------------------------------------------------------------------
+# Imports
+# -------------------------------------------------------------------------------------------------
+
 import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
@@ -9,10 +13,19 @@ from iblutil.util import Bunch
 
 import one.alf.io as alfio
 
-DATA_PATH = Path(r'C:\Users\Mayo\Downloads\FlatIron\website')
+
+# -------------------------------------------------------------------------------------------------
+# Constants
+# -------------------------------------------------------------------------------------------------
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = ROOT_DIR / 'data'
 
 
-# LOADING FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Loading functions
+# -------------------------------------------------------------------------------------------------
+
 def load_clusters(pid):
     clusters = alfio.load_object(DATA_PATH.joinpath(pid), object='clusters')
     return clusters
@@ -35,12 +48,16 @@ def load_trials(pid):
 
 def load_cluster_waveforms(pid):
     wfs = np.load(DATA_PATH.joinpath(pid, 'clusters.waveforms.npy'))
-    wf_chns = np.load(DATA_PATH.joinpath(pid, 'clusters.waveformsChannels.npy'))
+    wf_chns = np.load(DATA_PATH.joinpath(
+        pid, 'clusters.waveformsChannels.npy'))
 
     return wfs, wf_chns
 
 
-# FILTERING FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Filtering
+# -------------------------------------------------------------------------------------------------
+
 def _filter(obj, idx):
     obj = Bunch(copy.deepcopy(obj))
     for key in obj.keys():
@@ -48,12 +65,15 @@ def _filter(obj, idx):
 
     return obj
 
+
 def filter_spikes_by_good_clusters(spikes):
     return _filter(spikes, spikes.good)
+
 
 def filter_clusters_by_good_clusters(clusters):
     idx = np.where(clusters.label == 1)[0]
     return _filter(clusters, idx)
+
 
 def filter_spikes_by_cluster_idx(spikes, cluster_idx):
     idx = np.where(spikes.clusters == cluster_idx)[0]
@@ -62,7 +82,8 @@ def filter_spikes_by_cluster_idx(spikes, cluster_idx):
 
 
 def filter_spikes_by_trial(spikes, tstart, tend):
-    idx = np.where(np.bitwise_and(spikes.times >= tstart, spikes.times <= tend))[0]
+    idx = np.where(np.bitwise_and(
+        spikes.times >= tstart, spikes.times <= tend))[0]
     return _filter(spikes, idx)
 
 
@@ -87,7 +108,10 @@ def filter_wfs_by_cluster_idx(waveforms, waveform_channels, clusters, cluster_id
     return waveforms[idx], waveform_channels[idx]
 
 
-# PROCESSING FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Processing functions
+# -------------------------------------------------------------------------------------------------
+
 def bin_spikes(spike_times, align_times, pre_time, post_time, bin_size, weights=None):
 
     n_bins_pre = int(np.ceil(pre_time / bin_size))
@@ -99,7 +123,8 @@ def bin_spikes(spike_times, align_times, pre_time, post_time, bin_size, weights=
     bins = np.zeros(shape=(align_times.shape[0], n_bins))
 
     for i, (ep, t) in enumerate(zip(epoch_idxs, ts)):
-        xind = (np.floor((spike_times[ep[0]:ep[1]] - t[0]) / bin_size)).astype(np.int64)
+        xind = (
+            np.floor((spike_times[ep[0]:ep[1]] - t[0]) / bin_size)).astype(np.int64)
         w = weights[ep[0]:ep[1]] if weights is not None else None
         r = np.bincount(xind, minlength=tscale.shape[0], weights=w)
         bins[i, :] = r[:-1]
@@ -109,7 +134,10 @@ def bin_spikes(spike_times, align_times, pre_time, post_time, bin_size, weights=
     return bins, tscale
 
 
-# STYLING FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Styling functions
+# -------------------------------------------------------------------------------------------------
+
 def set_figure_style(fig, margin_inches=0.8):
     x_inches, y_inches = fig.figure.get_size_inches()
     fig.subplots_adjust(margin_inches / x_inches, margin_inches / y_inches)
@@ -127,7 +155,10 @@ def set_axis_style(ax, **kwargs):
     return ax
 
 
-# PLOTTING FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Plotting functions
+# -------------------------------------------------------------------------------------------------
+
 def plot_session_raster(spikes, trials, cluster_idx, trial_idx, subsample=100):
 
     alpha = spikes.sizes / np.max(spikes.sizes)
@@ -140,7 +171,8 @@ def plot_session_raster(spikes, trials, cluster_idx, trial_idx, subsample=100):
 
     # TODO Allen colours
     spikes = filter_spikes_by_cluster_idx(spikes, cluster_idx)
-    ax.scatter(spikes.times, spikes.depths, s=spikes.sizes, facecolors='none', edgecolors='r')
+    ax.scatter(spikes.times, spikes.depths, s=spikes.sizes,
+               facecolors='none', edgecolors='r')
     ax = set_axis_style(ax, xlabel='Time (s)', ylabel='Depth (um)')
 
     trials = filter_trials_by_trial_idx(trials, trial_idx)
@@ -152,11 +184,13 @@ def plot_session_raster(spikes, trials, cluster_idx, trial_idx, subsample=100):
 def plot_trial_raster(spikes, trials, cluster_idx, trial_idx):
 
     trials = filter_trials_by_trial_idx(trials, trial_idx)
-    spikes = filter_spikes_by_trial(spikes, trials['stimOn_times'] - 2, trials['feedback_times'] + 2)
+    spikes = filter_spikes_by_trial(
+        spikes, trials['stimOn_times'] - 2, trials['feedback_times'] + 2)
     alpha = spikes.sizes / np.max(spikes.sizes)
     fig, ax = plt.subplots(1, 1, figsize=(9, 6))
     set_figure_style(fig)
-    ax.scatter(spikes.times, spikes.depths, s=spikes.sizes, alpha=alpha, c='grey')
+    ax.scatter(spikes.times, spikes.depths,
+               s=spikes.sizes, alpha=alpha, c='grey')
     ax.set_ylim(0, 3840)
     ax.set_xlim(np.min(spikes.times), np.max(spikes.times))
 
@@ -177,7 +211,8 @@ def add_trial_events_to_raster(ax, trials):
 
     for e, c, l in zip(events, colors, labels):
         ax.axvline(trials[e], *ax.get_ylim(), c=c)
-        ax.text(trials[e], 1.01, l, c=c, rotation=45, rotation_mode='anchor', ha='left', transform=trans)
+        ax.text(trials[e], 1.01, l, c=c, rotation=45,
+                rotation_mode='anchor', ha='left', transform=trans)
 
     return ax
 
@@ -188,7 +223,8 @@ def plot_spikes_amp_vs_depth(clusters, cluster_idx):
     set_figure_style(fig)
     ax.set_ylim(0, 3840)
     ax.set_xlim(-5, 500)
-    ax.scatter(clusters.amps * 1e6, clusters.depths, facecolors='none', edgecolors='grey')
+    ax.scatter(clusters.amps * 1e6, clusters.depths,
+               facecolors='none', edgecolors='grey')
     clusters = filter_clusters_by_cluster_idx(clusters, cluster_idx)
     ax.scatter(clusters.amps * 1e6, clusters.depths, c='r')
     set_axis_style(ax, xlabel='Amplitude (uV)', ylabel='Depth (um)')
@@ -201,7 +237,8 @@ def plot_spikes_fr_vs_depth(clusters, cluster_idx):
     set_figure_style(fig)
     ax.set_ylim(0, 3840)
     ax.set_xlim(-2, 30)
-    ax.scatter(clusters.firing_rate, clusters.depths, facecolors='none', edgecolors='grey')
+    ax.scatter(clusters.firing_rate, clusters.depths,
+               facecolors='none', edgecolors='grey')
     clusters = filter_clusters_by_cluster_idx(clusters, cluster_idx)
     ax.scatter(clusters.firing_rate, clusters.depths, c='r')
     set_axis_style(ax, xlabel='Firing Rate (Hz)', ylabel='Depth (um)')
@@ -212,11 +249,13 @@ def plot_left_right_single_cluster_raster(spikes, trials, cluster_idx):
 
     spikes = filter_spikes_by_cluster_idx(spikes, cluster_idx)
     trial_idx, dividers = find_trial_ids(trials, sort='side')
-    fig, ax = single_cluster_raster(spikes.times, trials['stimOn_times'], trial_idx, dividers, ['g', 'y'], ['left', 'right'])
+    fig, ax = single_cluster_raster(
+        spikes.times, trials['stimOn_times'], trial_idx, dividers, ['g', 'y'], ['left', 'right'])
     set_figure_style(fig)
     ax[0].set_ylim(0, 20)  # TODO this shouldn't be harcoded here
     ax[1].set_yticklabels([])
-    set_axis_style(ax[1], xlabel='Time from Stim On (s)', ylabel='Sorted Trial Number')
+    set_axis_style(ax[1], xlabel='Time from Stim On (s)',
+                   ylabel='Sorted Trial Number')
     set_axis_style(ax[0], ylabel='Firing Rate (Hz)')
 
     return fig
@@ -231,7 +270,8 @@ def plot_correct_incorrect_single_cluster_raster(spikes, trials, cluster_idx):
     set_figure_style(fig)
     ax[0].set_ylim(0, 20)  # TODO this shouldn't be harcoded here
     ax[1].set_yticklabels([])
-    set_axis_style(ax[1], xlabel='Time from Feedback (s)', ylabel='Sorted Trial Number')
+    set_axis_style(ax[1], xlabel='Time from Feedback (s)',
+                   ylabel='Sorted Trial Number')
     set_axis_style(ax[0], ylabel='Firing Rate (Hz)')
 
 
@@ -241,17 +281,22 @@ def single_cluster_raster(spike_times, events, trial_idx, dividers, colors, labe
     post_time = 1
     raster_bin = 0.01
     psth_bin = 0.05
-    raster, t_raster = bin_spikes(spike_times, events, pre_time=pre_time, post_time=post_time, bin_size=raster_bin)
-    psth, t_psth = bin_spikes(spike_times, events, pre_time=pre_time, post_time=post_time, bin_size=psth_bin)
+    raster, t_raster = bin_spikes(
+        spike_times, events, pre_time=pre_time, post_time=post_time, bin_size=raster_bin)
+    psth, t_psth = bin_spikes(
+        spike_times, events, pre_time=pre_time, post_time=post_time, bin_size=psth_bin)
 
     dividers = [0] + dividers + [len(trial_idx)]
-    fig, ax = plt.subplots(2, 1, figsize=(4, 6), gridspec_kw={'height_ratios': [1, 3], 'hspace': 0}, sharex=True)
+    fig, ax = plt.subplots(2, 1, figsize=(4, 6), gridspec_kw={
+                           'height_ratios': [1, 3], 'hspace': 0}, sharex=True)
     for iD in range(len(dividers) - 1):
-        psth_div = np.nanmean(psth[trial_idx[dividers[iD]:dividers[iD + 1]]], axis=0) / psth_bin
+        psth_div = np.nanmean(
+            psth[trial_idx[dividers[iD]:dividers[iD + 1]]], axis=0) / psth_bin
         std_div = (np.nanstd(psth[trial_idx[dividers[iD]:dividers[iD + 1]]], axis=0) / psth_bin) \
-                  / np.sqrt(dividers[iD + 1] - dividers[iD])
+            / np.sqrt(dividers[iD + 1] - dividers[iD])
 
-        ax[0].fill_between(t_psth, psth_div - std_div, psth_div + std_div, alpha=0.4, color=colors[iD])
+        ax[0].fill_between(t_psth, psth_div - std_div,
+                           psth_div + std_div, alpha=0.4, color=colors[iD])
         ax[0].plot(t_psth, psth_div, alpha=1, color=colors[iD])
 
     ax[1].imshow(raster[trial_idx], cmap='binary', origin='lower',
@@ -262,12 +307,13 @@ def single_cluster_raster(spike_times, events, trial_idx, dividers, colors, labe
     for iD in range(len(dividers) - 1):
         ax[1].fill_between([post_time + raster_bin / 2, post_time + raster_bin / 2 + width],
                            [dividers[iD + 1], dividers[iD + 1]], [dividers[iD], dividers[iD]], color=colors[iD])
-        label_pos.append((dividers[iD + 1]- dividers[iD]) / 2 + dividers[iD])
+        label_pos.append((dividers[iD + 1] - dividers[iD]) / 2 + dividers[iD])
 
     secax = ax[1].secondary_yaxis('right')
 
     secax.set_yticks(label_pos)
-    secax.set_yticklabels(labels, rotation=90, rotation_mode='anchor', ha='center')
+    secax.set_yticklabels(labels, rotation=90,
+                          rotation_mode='anchor', ha='center')
     ax[1].set_xlim([-1 * pre_time, post_time + raster_bin / 2 + width])
     for ic, c in enumerate(colors):
         secax.get_yticklabels()[ic].set_color(c)
@@ -285,11 +331,13 @@ def single_cluster_raster(spike_times, events, trial_idx, dividers, colors, labe
 
 def plot_cluster_waveforms(waveforms, waveform_channels, channels, clusters, cluster_idx):
 
-    wfs, wf_chns = filter_wfs_by_cluster_idx(waveforms, waveform_channels, clusters, cluster_idx)
+    wfs, wf_chns = filter_wfs_by_cluster_idx(
+        waveforms, waveform_channels, clusters, cluster_idx)
 
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
     set_figure_style(fig)
-    wf_chn_pos = np.c_[channels['localCoordinates'][:, 0][wf_chns], channels['localCoordinates'][:, 1][wf_chns]]
+    wf_chn_pos = np.c_[channels['localCoordinates'][:, 0]
+                       [wf_chns], channels['localCoordinates'][:, 1][wf_chns]]
 
     wf_chn_pos[:, 0] = wf_chn_pos[:, 0] - np.min(wf_chn_pos[:, 0])
     wf_chn_pos[:, 1] = wf_chn_pos[:, 1] - np.min(wf_chn_pos[:, 1])
@@ -325,7 +373,10 @@ def plot_autocorrelogram(spikes, cluster_idx):
     return fig
 
 
-# CHANGE PLOT FUNCTIONS
+# -------------------------------------------------------------------------------------------------
+# Interactivity functions
+# -------------------------------------------------------------------------------------------------
+
 def on_pid_change(pid, cluster_idx, trial_idx):
     spikes = load_spikes(pid)
     clusters = load_clusters(pid)
@@ -342,10 +393,13 @@ def on_pid_change(pid, cluster_idx, trial_idx):
     # fig = plot_lfp_spectrum() # TODO
     figs.append(plot_spikes_amp_vs_depth(clusters, cluster_idx))
     figs.append(plot_spikes_fr_vs_depth(clusters, cluster_idx))
-    figs.append(plot_left_right_single_cluster_raster(spikes, trials, cluster_idx))
-    figs.append(plot_correct_incorrect_single_cluster_raster(spikes, trials, cluster_idx))
+    figs.append(plot_left_right_single_cluster_raster(
+        spikes, trials, cluster_idx))
+    figs.append(plot_correct_incorrect_single_cluster_raster(
+        spikes, trials, cluster_idx))
     # fig = plot_contrast_single_cluster_raster() # TODO
-    figs.append(plot_cluster_waveforms(waveforms, waveform_channels, channels, clusters, cluster_idx))
+    figs.append(plot_cluster_waveforms(
+        waveforms, waveform_channels, channels, clusters, cluster_idx))
     figs.append(plot_autocorrelogram(spikes, cluster_idx))
     # fig = plot_interspike_intreval() # TODO
 
@@ -376,10 +430,13 @@ def on_cluster_change(pid, cluster_idx, trial_idx):
     figs.append(plot_trial_raster(spikes, trials, cluster_idx, trial_idx))
     figs.append(plot_spikes_amp_vs_depth(clusters, cluster_idx))
     figs.append(plot_spikes_fr_vs_depth(clusters, cluster_idx))
-    figs.append(plot_left_right_single_cluster_raster(spikes, trials, cluster_idx))
-    figs.append(plot_correct_incorrect_single_cluster_raster(spikes, trials, cluster_idx))
+    figs.append(plot_left_right_single_cluster_raster(
+        spikes, trials, cluster_idx))
+    figs.append(plot_correct_incorrect_single_cluster_raster(
+        spikes, trials, cluster_idx))
     # fig = plot_contrast_single_cluster_raster() # TODO
-    figs.append(plot_cluster_waveforms(waveforms, waveform_channels, channels, clusters, cluster_idx))
+    figs.append(plot_cluster_waveforms(
+        waveforms, waveform_channels, channels, clusters, cluster_idx))
     figs.append(plot_autocorrelogram(spikes, cluster_idx))
     # fig = plot_interspike_intreval() # TODO
 
