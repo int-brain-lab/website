28779319999e85cd79d57fd863f76583480b9fa2
Cyrille Rossant <rossant@users.noreply.github.com>
2022-07-19 19:04:59 +0200

    Add Flask-based file-system caching

--- flaskapp.py
+++ flaskapp.py
@@ -14,6 +14,7 @@ import time
 from uuid import UUID
 
 from flask_cors import CORS  # , cross_origin
+from flask_caching import Cache
 from flask import Flask, render_template, send_file  # , session, request
 import numpy as np
 import matplotlib as mpl
@@ -123,9 +124,15 @@ def is_valid_uuid(uuid_to_test, version=4):
 # -------------------------------------------------------------------------------------------------
 # Server
 # -------------------------------------------------------------------------------------------------
+
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['CACHE_TYPE'] = 'FileSystemCache'
+app.config['CACHE_DEFAULT_TIMEOUT'] = 0
+app.config['CACHE_DIR'] = DATA_DIR / 'cache'
 CORS(app, support_credentials=True)
+# app.config.from_mapping(aconfig)
+cache = Cache(app)
 
 
 # -------------------------------------------------------------------------------------------------
@@ -192,28 +199,33 @@ def main():
 
 
 @app.route('/api/session/<pid>/details')
+@cache.cached()
 def session_details(pid):
     DATACLASS.session_init(pid)
     return DATACLASS.get_session_details()
 
 
 @app.route('/api/session/<pid>/trial_details/<int:trial_idx>')
+@cache.cached()
 def trial_details(pid, trial_idx):
     return DATACLASS.get_trial_details(trial_idx)
 
 
 @app.route('/api/session/<pid>/cluster_details/<int:cluster_idx>')
+@cache.cached()
 def cluster_details(pid, cluster_idx):
     return DATACLASS.get_cluster_details(cluster_idx)
 
 
 @app.route('/api/session/<pid>/raster')
+@cache.cached()
 def raster(pid):
     fig = DATACLASS.plot_session_raster(DATACLASS.spikes)
     return send_figure(fig)
 
 
 @app.route('/api/session/<pid>/psychometric')
+@cache.cached()
 def psychometric_curve(pid):
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
     fig = DATACLASS.plot_psychometric_curve(ax=ax)
@@ -222,6 +234,7 @@ def psychometric_curve(pid):
 
 
 @app.route('/api/session/<pid>/clusters')
+@cache.cached()
 def cluster_good_bad_plot(pid):
     fig, ax = plt.subplots(1, 1, figsize=(4, 6))
     fig = DATACLASS.plot_good_bad_clusters(ax=ax)
@@ -230,6 +243,7 @@ def cluster_good_bad_plot(pid):
 
 
 @app.route('/api/session/<pid>/raster/trial/<int:trial_idx>')
+@cache.cached()
 def raster_with_trial(pid, trial_idx):
     fig, axs = plt.subplots(1, 2, figsize=(9, 6), gridspec_kw={'width_ratios': [10, 1], 'wspace': 0.05})
     DATACLASS.plot_session_raster(trial_idx=trial_idx, ax=axs[0])
@@ -239,6 +253,7 @@ def raster_with_trial(pid, trial_idx):
 
 
 @app.route('/api/session/<pid>/trial_raster/<int:trial_idx>')
+@cache.cached()
 def trial_raster(pid, trial_idx):
     fig, axs = plt.subplots(1, 2, figsize=(9, 6), gridspec_kw={'width_ratios': [10, 1], 'wspace': 0.05})
     DATACLASS.plot_trial_raster(trial_idx=trial_idx, ax=axs[0])
@@ -248,6 +263,7 @@ def trial_raster(pid, trial_idx):
 
 
 @app.route('/api/session/<pid>/cluster/<int:cluster_idx>')
+@cache.cached()
 def cluster_plot(pid, cluster_idx):
     fig, axs = plt.subplots(1, 3, figsize=(9, 6), gridspec_kw={'width_ratios': [4, 4, 1], 'wspace': 0.05})
     DATACLASS.plot_spikes_amp_vs_depth(cluster_idx, ax=axs[0])
@@ -259,6 +275,7 @@ def cluster_plot(pid, cluster_idx):
 
 
 @app.route('/api/session/<pid>/cluster_response/<int:cluster_idx>')
+@cache.cached()
 def cluster_response_plot(pid, cluster_idx):
     fig, axs = plt.subplots(2, 3, figsize=(9, 6), gridspec_kw={
         'height_ratios': [1, 3], 'hspace': 0, 'wspace': 0.1}, sharex=True)
@@ -277,7 +294,9 @@ def cluster_response_plot(pid, cluster_idx):
 
     return send_figure(fig)
 
+
 @app.route('/api/session/<pid>/cluster_properties/<int:cluster_idx>')
+@cache.cached()
 def cluster_properties_plot(pid, cluster_idx):
     axs = []
     fig = plt.figure(figsize=(9, 6))
