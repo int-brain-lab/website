b594546e29adbbe7529758e5295f89e07e6f5da9
Cyrille Rossant <rossant@users.noreply.github.com>
2022-09-19 10:31:30 +0200

    Update cache upload script

--- cache_upload.sh
+++ cache_upload.sh
@@ -1,6 +1,7 @@
-#python generator.py # for all sessions
+#rm cache.zip
+python generator.py # for all sessions
 #python generator.py <pid> # for 1 session
-
-zip -r cache.zip cache/
-scp -r cache.zip iblviz:~/
-ssh iblviz "mv ~/cache.zip /var/www/ibl_website/website && cd /var/www/ibl_website/website && unzip cache.zip"
+rsync -avzh cache iblviz:/var/www/ibl_website/website/cache
+#zip -r cache.zip cache/
+#scp -r cache.zip iblviz:~/
+#ssh iblviz "mv ~/cache.zip /var/www/ibl_website/website && cd /var/www/ibl_website/website && unzip cache.zip"
