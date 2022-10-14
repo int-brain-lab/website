32d6a075342e24c0537b1ca6f2d6b0af0bce5724
Dan Birman <danbirman@gmail.com>
2022-10-13 21:52:09 -0700

    Add test scripts

---
+++ TrialViewer/pipelines/convert_timestamps2index.py
@@ -0,0 +1,5 @@
+# This function goes through the data files (provided by Mayo's scripts) and converts
+# all of the timestamps into indexes into the video files
+
+# In Unity this then allows you to move through time using just the index of the frames
+# instead of having to handle rounding errors with the timestamps
