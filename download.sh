25765ba861d0e9bc9a074e3673beefd408ad0e75
Cyrille Rossant <rossant@users.noreply.github.com>
2022-12-22 17:50:22 +0100

    Update download script

--- download.sh
+++ download.sh
@@ -1,1 +1,3 @@
 rsync -avzh iblviz:/var/www/ibl_website/website/static/WebGL/ static/WebGL/
+rsync -avzh iblviz:/var/www/ibl_website/trialviewer_data/TrialViewerBuild/ static/TrialViewerBuild/
+rsync -avzh iblviz:/var/www/ibl_website/iblmini_data/Build/ static/Build/
