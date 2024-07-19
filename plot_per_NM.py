50c7728c34764b7ef1dff8c6b0314143fc2752be
KceniaB <kcenia.bougrova@research.fchampalimaud.org>
2024-07-19 11:12:30 +0100

    optimized; added functions and plot by contrast

--- plot_per_NM.py
+++ plot_per_NM.py
@@ -3,7 +3,10 @@
 KB loop through the good sessions 
 GOOD SESSIONS in excel MICE AND SESSIONS 
 
+Goal: make plots for more than 1 session
+        for example, joining all the DA sessions 
 """ 
+#%%
 #imports 
 import numpy as np 
 import pandas as pd
@@ -37,27 +40,32 @@ def get_eid(mouse,date):
 test_02 = pd.read_csv('/home/ibladmin/Downloads/Mice_GOOD_sorted.csv') 
 test_02['Date'] = pd.to_datetime(test_02['Date'], format='%m/%d/%Y')
 test_03 = test_02[['Mouse', 'Date', 'NM', 'region']] 
-EVENT = 'feedback_times'
-
-EXCLUDES = []  
-
-IMIN = 0
+EVENT = 'stimOnTrigger_times'
 
 # Initialize empty containers
 psth_combined = None
 df_trials_combined = pd.DataFrame()
 
-# test_04 = test_03[test_03["NM"]=="DA"].reset_index(drop=True)
+EXCLUDES = []  
+IMIN = 0
+
+test_04 = test_03[test_03["NM"]=="DA"].reset_index(drop=True)
 # test_04 = test_03[test_03["NM"]=="5HT"].reset_index(drop=True) 
 # test_04 = test_03[test_03["NM"]=="NE"].reset_index(drop=True)
-test_04 = test_03[test_03["NM"]=="ACh"].reset_index(drop=True)
+# test_04 = test_03[test_03["NM"]=="ACh"].reset_index(drop=True) 
 
+####################################
+#for DA: 
+EXCLUDES = [5,6,8,12]  
+IMIN = 0
+
+
+#%%
 for i in range(len(test_04)): 
     if i < IMIN:
         continue
     if i in EXCLUDES:
         continue
-    EVENT = "feedback_times"
     mouse = test_04.Mouse[i] 
     date = test_04.Date[i]
     if isinstance(date, pd.Timestamp):
@@ -71,7 +79,7 @@ for i in range(len(test_04)):
     df_trials["region"] = region
     df_trials["eid"] = eid 
 
-    path_initial = '/mnt/h0/kb/data/psth_npy/preprocess_calcium_jove2019_feedback_times_etc/' 
+    path_initial = f'/mnt/h0/kb/data/psth_npy/preprocess_calcium_jove2019_{EVENT}_etc/' 
     path = path_initial + f'preprocess_calcium_jove2019_{EVENT}_{mouse}_{date}_Region{region}G_{eid}.npy'
 
     # Load psth_idx from file
@@ -80,7 +88,7 @@ for i in range(len(test_04)):
     # Concatenate psth_idx arrays
     if psth_combined is None:
         psth_combined = psth_idx
-    else:
+    else: 
         psth_combined = np.hstack((psth_combined, psth_idx))
 
     # create allContrasts 
@@ -107,8 +115,8 @@ for i in range(len(test_04)):
 #%%
     ##################################################################################################
     # PLOT 
-    psth_good = psth_idx[:,(df_trials.feedbackType == 1)]
-    psth_error = psth_idx[:,(df_trials.feedbackType == -1)]
+    psth_good = psth_combined[:,(df_trials_combined.feedbackType == 1)]
+    psth_error = psth_combined[:,(df_trials_combined.feedbackType == -1)]
     # Calculate averages and SEM
     psth_good_avg = psth_good.mean(axis=1)
     sem_good = psth_good.std(axis=1) / np.sqrt(psth_good.shape[1])
@@ -148,14 +156,142 @@ for i in range(len(test_04)):
     ax4.set_ylabel('Average Value')
     ax4.set_xlabel('Time')
 
-    fig.suptitle(f'calcium_mad_{EVENT}_{mouse}_{date}_{region}_{eid}', y=1, fontsize=14)
+    fig.suptitle(f'jove2019_{EVENT}_{mouse}_{date}_{region}_{eid}', y=1, fontsize=14)
     plt.tight_layout()
     plt.show()
     ##################################################################################################
 
 
 #%%
-np.save('/mnt/h0/kb/data/psth_npy/preprocess_calcium_jove2019_feedback_times_etc/RESULTS/jove2019_psth_combined_ACh.npy', psth_combined)
-df_trials_combined.to_parquet('/mnt/h0/kb/data/psth_npy/preprocess_calcium_jove2019_feedback_times_etc/RESULTS/jove2019_df_trials_combined_ACh.pqt')
+""" SAVE THE PSTH AND DF_TRIALS """
+#save the psth npy and the df_trials pqt 
+# path_initial = '/mnt/h0/kb/data/psth_npy/preprocess_calcium_jove2019_feedback_times_etc/' 
+np.save(path_initial+f'RESULTS/jove2019_psth_combined_NE_{EVENT}.npy', psth_combined)
+df_trials_combined.to_parquet(path_initial+f'RESULTS/jove2019_df_trials_combined_NE_{EVENT}.pqt')
+
+#%%
+#import the saved files 
+what_to_load = 'combined_NE'
+test_psth = np.load(path_initial+f'RESULTS/jove2019_psth_{what_to_load}.npy')
+test_trials = pd.read_parquet(path_initial+f'RESULTS/jove2019_df_trials_{what_to_load}.pqt')
+
+# %%
+##################################################################################################
+""" MAKE DIFFERENT PLOTS """
+""" 0. functions """
+
+def avg_sem(psth_array): 
+    psth_avg = psth_array.mean(axis=1)
+    psth_sem = psth_array.std(axis=1) / np.sqrt(psth_array.shape[1])
+    return psth_avg, psth_sem
+
+def filter_data(psth_array=psth_combined, df_trials=df_trials_combined, event1=None, event2=None, event3=None, 
+                mice=None): 
+    if mouse!=None: 
+        mask = df_trials["mouse"].isin(mice)
+        test = psth_array[:, mask]
+    return test 
+    # test = filter_data(mice=["ZFM-04019","ZFM-04022"])
+
+    if event1=="feedbackType": 
+        psth_correct = psth_array[:,(df_trials[event1] == 1)]
+        psth_incorrect = psth_array[:,(df_trials[event1] == -1)] 
+    
+    if event2=="allContrasts": 
+        psth_100 = psth_array[:,(df_trials[event2] == 1)]
+        psth_25 = psth_array[:,(df_trials[event2] == 0.25)]
+        psth_12 = psth_array[:,(df_trials[event2] == 0.125)]
+        psth_06 = psth_array[:,(df_trials[event2] == 0.0625)]
+        psth_0 = psth_array[:,(df_trials[event2] == 0)] 
+    
+    if event3=="probabilityLeft": 
+        psth_50 = psth_array[:,(df_trials[event3] == 0.5)]
+        psth_20 = psth_array[:,(df_trials[event3] == 0.2)]
+        psth_80 = psth_array[:,(df_trials[event3] == 0.8)] 
+    
+    if event1=="feedbackType" and event2=="allContrasts": 
+
+    
+    return psth_correct, psth_incorrect
 
 
+
+
+
+
+
+
+""" 1. feedbackType """
+psth_good = psth_combined[:,(df_trials_combined.feedbackType == 1)] 
+psth_error = psth_combined[:,(df_trials_combined.feedbackType == -1)]
+# Calculate averages and SEM
+psth_good_avg = psth_good.mean(axis=1)
+sem_good = psth_good.std(axis=1) / np.sqrt(psth_good.shape[1])
+psth_error_avg = psth_error.mean(axis=1)
+sem_error = psth_error.std(axis=1) / np.sqrt(psth_error.shape[1])
+
+# Create the figure and gridspec
+fig = plt.figure(figsize=(10, 12))
+gs = fig.add_gridspec(2, 2, height_ratios=[3, 1])
+
+# Plot the heatmap and line plot for correct trials
+ax1 = fig.add_subplot(gs[0, 0])
+sns.heatmap(psth_good.T, cbar=False, ax=ax1) #, center = 0.0)
+ax1.invert_yaxis()
+ax1.axvline(x=30, color="white", alpha=0.9, linewidth=3, linestyle="dashed") 
+ax1.set_title('Correct Trials')
+
+ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)
+ax2.plot(psth_good_avg, color='#2f9c95', linewidth=3) 
+# ax2.plot(psth_good, color='#2f9c95', linewidth=0.1, alpha=0.2)
+ax2.fill_between(range(len(psth_good_avg)), psth_good_avg - sem_good, psth_good_avg + sem_good, color='#2f9c95', alpha=0.15)
+ax2.axvline(x=30, color="black", alpha=0.9, linewidth=3, linestyle="dashed")
+ax2.set_ylabel('Average Value')
+ax2.set_xlabel('Time')
+
+# Plot the heatmap and line plot for incorrect trials
+ax3 = fig.add_subplot(gs[0, 1], sharex=ax1)
+sns.heatmap(psth_error.T, cbar=False, ax=ax3) #, center = 0.0)
+ax3.invert_yaxis()
+ax3.axvline(x=30, color="white", alpha=0.9, linewidth=3, linestyle="dashed") 
+ax3.set_title('Incorrect Trials')
+
+ax4 = fig.add_subplot(gs[1, 1], sharex=ax3, sharey=ax2)
+ax4.plot(psth_error_avg, color='#d62828', linewidth=3)
+ax4.fill_between(range(len(psth_error_avg)), psth_error_avg - sem_error, psth_error_avg + sem_error, color='#d62828', alpha=0.15)
+ax4.axvline(x=30, color="black", alpha=0.9, linewidth=3, linestyle="dashed")
+ax4.set_ylabel('Average Value')
+ax4.set_xlabel('Time')
+
+fig.suptitle(f'calcium_mad_{EVENT}_{mouse}_{date}_{region}_{eid}', y=1, fontsize=14)
+plt.tight_layout()
+plt.show()
+
+#%%
+""" 2. allContrasts """
+
+psth_array=psth_combined
+df_trials=df_trials_combined 
+event2="allContrasts"
+psth_100 = psth_array[:,(df_trials[event2] == 1)]
+avg100, sem100 = avg_sem(psth_100)
+psth_25 = psth_array[:, (df_trials[event2] == 0.25)]
+avg25, sem25 = avg_sem(psth_25)
+psth_12 = psth_array[:, (df_trials[event2] == 0.125)]
+avg12, sem12 = avg_sem(psth_12)
+psth_06 = psth_array[:, (df_trials[event2] == 0.0625)]
+avg06, sem06 = avg_sem(psth_06)
+psth_0 = psth_array[:, (df_trials[event2] == 0)]
+avg0, sem0 = avg_sem(psth_0)
+
+plt.plot(avg100, color="black", label="100")
+plt.plot(avg25, color="black", alpha=0.6, label="25")
+plt.plot(avg12, color="black", alpha=0.4, label="12")
+plt.plot(avg06, color="black", alpha=0.2, label="6")
+plt.plot(avg0, color="black", alpha=0.05, label="0") 
+plt.axvline(x=30, linestyle='dashed', color='black')
+plt.legend()
+plt.title(f'All contrasts, aligned to {EVENT}') 
+plt.show()
+##################################################################################################
+# %%
