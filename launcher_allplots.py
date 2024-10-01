79e8087161e001142e9165aec3e789773e8de75a
owinter <olivier.winter@hotmail.fr>
2024-10-01 16:17:25 +0100

    update plot generation for newer set of recordings

--- launcher_allplots.py
+++ launcher_allplots.py
@@ -1,38 +1,36 @@
-
-# %%
 from one.api import ONE
 from pathlib import Path
-from generator import Generator
-import traceback
+from generator import Generator, make_data_js
 import joblib
-from iblutil.util import setup_logger
+import logging
+import traceback
 
-logger = setup_logger()
-
+logger = logging.getLogger('ibllib')
 
 one = ONE(base_url='https://alyx.internationalbrainlab.org', cache_dir="/mnt/h0/kb/data/one")
-SAVE_PATH = Path("/mnt/h0/kb/viz_figures_new")
+SAVE_PATH = Path("/mnt/h0/kb/viz_figures")
 SAVE_PATH.mkdir(exist_ok=True, parents=True)
 
 photometry_data = list(Path('/mnt/h0/kb/data/one/mainenlab/Subjects').rglob('raw_photometry.pqt'))
 photometry_sessions = [str(ph.parent.parent.parent) for ph in photometry_data]
 photometry_sessions = list(set(photometry_sessions))
+len(photometry_data)
+len(photometry_sessions)
+
 
 def make_session_plots(session):
     errored = []
-    try:
-        session = Path(session)
-        eid = one.path2eid(session)
-        g = Generator(eid, one=one, data_path=session, cache_path=SAVE_PATH)
-        g.make_all_plots(nums=(1, 2, 3, 4))
-    except Exception as err:
-        # logger.error(f"Error in session {session} \n {traceback.format_exc()}")
-        # raise err
-        errored.append({session: err})
+    session = Path(session)
+    eid = one.path2eid(session)
+    g = Generator(eid, one=one, data_path=session, cache_path=SAVE_PATH)
+    g.make_all_plots(nums=(1, 2, 3, 4))
 
-for session in photometry_sessions:
+
+# make_session_plots(session)
+# 51 first movement times error /mnt/h0/kb/data/one/mainenlab/Subjects/ZFM-03448/2021-12-10/
+IMIN = 52
+for i, session in enumerate(photometry_sessions):
+    if i < IMIN:
+        continue
     make_session_plots(session)
 # joblib.Parallel(n_jobs=10)(joblib.delayed(make_session_plots)(session) for session in photometry_sessions)
-# make_session_plots(session='/mnt/h0/kb/data/one/mainenlab/Subjects/ZFM-05236/2023-06-27/001') # good example session
-
-# cf. make_data_js.py for the next steps to put the website online
